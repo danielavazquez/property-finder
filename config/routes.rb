c81265de5347cecc3b283c50fe523396c24c065c
daniela <daniela_056@hotmail.com>
2021-11-11 15:41:33 -0700

    Add webpacker via yarn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'public/main'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'public#main'
 end
