d34f67aa064d3c3bbf399758a76d805c64b01db5
daniela <daniela_056@hotmail.com>
2021-11-18 10:08:39 -0700

    Add bootstap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 import 'bootstrap/dist/js/bootstrap'
-import "bootstrap/dist/css/bootstrap";
+import 'bootstrap/dist/css/bootstrap'
 require("@rails/activestorage").start()
 require("channels")
 
